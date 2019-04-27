4a90dd7cb29b4f2504bc8adb4bf39775c20c4234
marcoshenriquemaia <50059412+marcoshenriquemaia@users.noreply.github.com>
2019-04-27 18:40:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+// function nome(valor1, tipo1, quantidade1) {
+//     let quantidadeASerFeita = 1;
+//     if (tipo1 == 'real') {
+//         quantidadeASerFeita = quantidade1;
+//     }
+//     for (let valorInicial = 0; valorInicial < quantidadeASerFeita; valorInicial = valorincial + 1) {
+//         console.log(valor1 + tipo1)
+//     }
+// }
+var usuarios = [
+    {
+        nome: "Diego",
+        habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        nome: "Gabriel",
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    },
+    {
+        nome: "asdf",
+        habilidades: ["VueJS", "Ruby on qwer", "489qw"]
+    },
+    {
+        nome: "Gabr12312iel",
+        habilidades: ["asdfe", "aw on Rails", "Elixir"]
+    }
+];
+// for (usuario of usuarios){
+//     console.log(
+//         "O " + usuario.nome + " possui as habilidades: " + usuario.habilidades.join(', ')
+//      )
+// }
+// const a = {
+//     chave1: 1,
+//     chave2: 2
+// }
+
+const b = a.chave1
+
+
+
+// O Diego possui as habilidades: Javascript, ReactJS, Redux
+// O Gabriel possui as habilidades: VueJS, Ruby on Rails, Elixir
+
+// for(let valorInicial = 0; valorInicial <= usuarios.length )
+
+
+// faça isso aqui nessas condições e para quando não obedecer ( determine o valor inicial como = 0;
+// valor inical == 0 verifica pra mim se ele é menor do que quantidade a ser feita [olha cara seu valor é 0 e quantidade é 1, enão 0 é menor que 1 == true]
+// mostra ai o que ta dentro do seu escopo [console.log(valor1)] adiciona mais 1 ae no valor inicial pra eu testar dnv valor inicial + 1
+// valor inial agora é 1, 1 é menor do que 1? não cara é igual == false ok vou pra proxima)
+
+// console.log(
+//     "O " +
+//       usuario.nome +
+//       " possui as habilidades: " +
+//       usuario.habilidades.join(", ")
+
+
+const valor = 750;
+const tipo = 'real';
+const quantidade = 3;
+
+// nome(valor, tipo, quantidade)
